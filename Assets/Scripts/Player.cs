4709f78b3d5593f77ad97bca0e580f0328d1974b
danio24 <danio.quero@gmail.com>
2021-06-22 11:16:30 +0200

    shooting cooldown

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,8 +10,18 @@ public class Player : MonoBehaviour
     [SerializeField]
     private Vector2 _direction = Vector2.zero;
 
+    [SerializeField]
+    private GameObject _laserPrefab = null;
+
     private CameraBounds _cameraBounds = null;
     private Vector3 _playerPosition = Vector3.zero;
+
+    private SpriteRenderer _spriteRenderer;
+    private Vector2 _laserSpawnPosition = new Vector2();
+
+    [SerializeField]
+    private float _fireRate = 0.2f;
+    private float _nextFireTime = -1f;
 
     // Start is called before the first frame update
     void Start()
@@ -21,6 +31,8 @@ public class Player : MonoBehaviour
             _cameraBounds = camObj.GetComponent<CameraBounds>();
         else
             Debug.LogError("Camera not found");
+
+        _spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -28,6 +40,7 @@ public class Player : MonoBehaviour
     {
         CheckBounds();
         Move();
+        ShootLaser();
     }
 
     private void Move()
@@ -42,12 +55,30 @@ public class Player : MonoBehaviour
 
     private void CheckBounds()
     {
+        float yOff = _spriteRenderer.sprite.rect.size.y / 2 / _spriteRenderer.sprite.pixelsPerUnit * transform.lossyScale.y;
+        float xOff = _spriteRenderer.sprite.rect.size.x / 2 / _spriteRenderer.sprite.pixelsPerUnit * transform.lossyScale.x;
+
         if (_cameraBounds != null)
             _playerPosition.Set(
-                Mathf.Clamp(transform.position.x, -_cameraBounds.CameraVisual.x, _cameraBounds.CameraVisual.x),
-                Mathf.Clamp(transform.position.y, -_cameraBounds.CameraVisual.y, _cameraBounds.CameraVisual.y),
+                Mathf.Clamp(transform.position.x, -_cameraBounds.CameraVisual.x + xOff, _cameraBounds.CameraVisual.x - xOff),
+                Mathf.Clamp(transform.position.y, -_cameraBounds.CameraVisual.y + yOff, _cameraBounds.CameraVisual.y - yOff),
                 transform.position.z);
 
         transform.position = _playerPosition;
+    }
+
+    private void ShootLaser()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _laserSpawnPosition = transform.position;
+            _laserSpawnPosition.y += _spriteRenderer.sprite.rect.size.y / 2 / _spriteRenderer.sprite.pixelsPerUnit * transform.lossyScale.y;
+
+            if (_laserPrefab != null && Time.time > _nextFireTime)
+            {
+                _nextFireTime = Time.time + _fireRate;
+                Instantiate(_laserPrefab, _laserSpawnPosition, Quaternion.identity);
+            }
+        }
     }
 }
