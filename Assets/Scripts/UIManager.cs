c05d8b3a8020d2e145d56e80415055f0617fd6d0
danio24 <danio.quero@gmail.com>
2021-07-07 16:42:31 +0200

    restart game

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -2,12 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
     private Text _score;
     private Text _gameover;
+    private Text _restart;
     private Image _livesDisplay;
+
+    private bool _isGameOver = false;
 
     [SerializeField]
     private Sprite[] _livesSprite = new Sprite[4];
@@ -25,9 +29,23 @@ public class UIManager : MonoBehaviour
 
         _gameover.enabled = false;
 
+        _restart = transform.Find("Restart").GetComponent<Text>();
+        if (_restart == null)
+            Debug.LogError("No Restart Text found");
+
+        _restart.enabled = false;
+
         _livesDisplay = transform.Find("LivesDisplay").GetComponent<Image>();
         if (_livesDisplay == null)
             Debug.LogError("No Lives Sprite found");
+    }
+
+    private void Update()
+    {
+        if (_isGameOver && Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene("Level_1");
+        }
     }
 
     public void UpdateScoreText(int score)
@@ -44,6 +62,8 @@ public class UIManager : MonoBehaviour
         {
             _livesDisplay.enabled = false;
             StartCoroutine(GameOverFlick());
+            _restart.enabled = true;
+            _isGameOver = true;
         }
     }
 
